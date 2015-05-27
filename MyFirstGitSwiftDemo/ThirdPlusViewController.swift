a2289591f2f9afe45c7297bb9dd7c92529a0d0c3
luosong <luosong@luosongdeMacBook-Pro.local>
2015-05-27 21:58:01 +0800

    First time commit, the init demo.

---
+++ MyFirstGitSwiftDemo/ThirdPlusViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdPlusViewController.swift
+//  MyFirstGitSwiftDemo
+//
+//  Created by luosong on 15/5/27.
+//  Copyright (c) 2015年 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ThirdPlusViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
