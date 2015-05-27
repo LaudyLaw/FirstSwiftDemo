d510c60eb9324c105aa79d3677ce74f9cdbda003
luosong <luosong@192.168.0.117>
2015-05-27 20:38:51 +0800

    Initial Commit

---
+++ MyFirstGitSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstGitSwiftDemo
+//
+//  Created by luosong on 15/5/27.
+//  Copyright (c) 2015年 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
